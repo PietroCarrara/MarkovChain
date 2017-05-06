5ba9305914e618ed616b2fc53d8df07b26be77f4
Pietro Carrara <piticarrara@gmail.com>
2017-05-06 12:47:07 +0000

    Initial Commit

---
+++ Classes/DictionaryPool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MarkovChain.Classes
+{
+    public class DictionaryPool
+    {
+        List<Dictionary> keys;
+    }
+}
---
+++ Classes/Key.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MarkovChain.Classes
+{
+    public class Dictionary
+    {
+        public string Key { get; private set; }
+
+        public string[] Values { get; private set; }
+    }
+}
