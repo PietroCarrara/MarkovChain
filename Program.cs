efe6b3bb8b09504b06193fbe196e567f0d2fd8f8
Pietro Carrara <piticarrara@gmail.com>
2017-05-06 13:36:17 +0000

    Updated The Testing Program

--- Program.cs
+++ Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MarkovChain.Classes;
+
 namespace MarkovChain
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DictionaryPool pool = new DictionaryPool();
+
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+
+            for (int i = 0; i < words.Length - 2; i++)
+            {
+                Dictionary sla = new Dictionary(words[i] + " " + words[i + 1], words[i + 2]);
+
+                // PrintDic(sla);
+
+                pool.Add(sla);
+            }
+
+            foreach (var dic in pool.Dics)
+            {
+                PrintDic(dic);
+            }
+        }
+
+        static void PrintDic(Dictionary dic)
+        {
+            Console.Write('"' + dic.Key + "\": [");
+
+            Console.Write(dic.Values[0]);
+
+            for (int i = 1; i < dic.Values.Count; i++)
+            {
+                Console.Write(", " + dic.Values[i]);
+            }
+
+            Console.WriteLine("]");
         }
     }
 }
