f2428c3dc9f1290579c6f11e91ae4081bfe8875e
Pietro Carrara <piticarrara@gmail.com>
2017-05-06 13:22:34 +0000

    Added Methods To Add Values To The Dictionary Pool

--- Classes/DictionaryPool.cs
+++ Classes/DictionaryPool.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,6 +9,55 @@ namespace MarkovChain.Classes
 {
     public class DictionaryPool
     {
-        List<Dictionary> keys;
+        List<Dictionary> Dics;
+
+        public DictionaryPool()
+        {
+            Dics = new List<Dictionary>();
+        }
+
+        public void Add(Dictionary dic)
+        {
+            foreach (var dict in Dics)
+            {
+                if (dic.Key == dict.Key)
+                {
+                    dict.AddRange(dic.Values);
+                    return;
+                }
+            }
+
+            Dics.Add(dic);
+        }
+
+        public void AddRange(IEnumerable<Dictionary> dics)
+        {
+            foreach (var dic in dics)
+            {
+                foreach (var dict in Dics)
+                {
+                    if (dic.Key == dict.Key)
+                    {
+                        dict.AddRange(dic.Values);
+
+                        // If we added the dictionary, continue to the next
+                        continue;
+                    }
+                }
+                Dics.Add(dic);
+            }
+        }
+
+        public Dictionary this[string key]
+        {
+            get
+            {
+                foreach (var dic in Dics)
+                    if (dic.Key == key)
+                        return dic;
+
+                return null;
+            }
+        }
     }
 }
